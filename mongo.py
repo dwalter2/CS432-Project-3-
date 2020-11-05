706bf58875fb27649173fa2a6c616fc24eab06d0
dwalter2 <32689604+dwalter2@users.noreply.github.com>
2020-11-04 21:00:26 -0500

    Update mongo.py

--- mongo.py
+++ mongo.py
@@ -1,9 +1,14 @@
 import pymongo
+import datetime
+import pprint
 from pymongo import MongoClient
 
 def da():
-    client = MongoClient("mongodb+srv://dwalter2:mongodavid@cluster0.daf4k.mongodb.net/sample_airbnb?retryWrites=true&w=majority")
-    db = client["sample_airbnb"]
-    db1 = db["listingAndReviews"]
-    print(db1.collectionName)
+    client = MongoClient("mongodb+srv://dwalter2:mongodavid@cluster0.daf4k.mongodb.net/TwitterData?retryWrites=true&w=majority")
+    db = client["TwitterData"]
+    db1 = db["trigrams"]
+    query = { 'gram' : 'covid 19 pandemic'}
+    mydoc = db1.find(query)
+    for x in mydoc:
+        pprint.pprint(x)
 da()
