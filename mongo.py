4a70da86188ca0611890dde71b092e8651d560cb
dwalter2 <32689604+dwalter2@users.noreply.github.com>
2020-11-14 14:56:17 -0500

    Update mongo.py

--- mongo.py
+++ mongo.py
@@ -1,14 +1,20 @@
 import pymongo
 import datetime
+import os
 import pprint
+import csv
+import pandas as pd
 from pymongo import MongoClient
 
 def da():
     client = MongoClient("mongodb+srv://dwalter2:mongodavid@cluster0.daf4k.mongodb.net/TwitterData?retryWrites=true&w=majority")
-    db = client["TwitterData"]
-    db1 = db["trigrams"]
-    query = { 'gram' : 'covid 19 pandemic'}
-    mydoc = db1.find(query)
-    for x in mydoc:
-        pprint.pprint(x)
+    db = client["CovidData"]
+    db1 = db["DayTotals"]
+    query = { "$max" : "New Cases"}
+    #v = db1.find( {"$max" : "$New Cases"})
+    v = list(db1.find().sort([("New Cases",-1)]).limit(1))
+    print(v[0]["Date"].strftime("%m %d %Y"))
+    x = list(db1.find().sort([("New Cases",1)]).limit(1))
+    print(x[0]["New Cases"])
+
 da()
